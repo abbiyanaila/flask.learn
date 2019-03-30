854d7047b9a64872955c89855130f5f3be443d3d
abbiyanaila <abbiyanaila@gmail.com>
2019-03-31 00:25:53 +0800

    feat: add functions of delete and put

--- restful/app.py
+++ restful/app.py
@@ -23,16 +23,31 @@ class Item(Resource):
 
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None):
-            return{'massage': "An item with name '{}' already exists.".format(name)}, 400
+            return {'massage': "An item with name '{}' already exists.".format(name)}, 400
 
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'massage': 'Item delete'}
+
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
 class ItemList(Resource):
     def get(self):
-        return{'items': items}
+        return {'items': items}
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
