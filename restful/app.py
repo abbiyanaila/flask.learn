bce1d1840c034fe4e8926eee26dcd21b63d6d318
abbiyanaila <abbiyanaila@gmail.com>
2019-03-31 00:04:39 +0800

    feat: add new files security and user

--- restful/app.py
+++ restful/app.py
@@ -1,21 +1,28 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'akasht'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 class Item(Resource):
+    @jwt_required
     def get(self, name):
-        item = next(filter(lambda x:x['name'] == name, items), None)
+        item = next(filter(lambda x: x['name'] == name, items), None)
         # for item in items:
         #     if item['name'] == name:
         #         return item
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        if next(filter(lambda x:x['name'] == name, items), None)
+        if next(filter(lambda x: x['name'] == name, items), None):
             return{'massage': "An item with name '{}' already exists.".format(name)}, 400
 
         data = request.get_json()
@@ -31,5 +38,3 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 
 app.run(port=8400, debug=True)
-
-
