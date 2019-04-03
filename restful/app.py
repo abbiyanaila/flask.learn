189da4ef1bfd9480212b8106f141c2e2c2c34ee9
abbiyanaila <abbiyanaila@gmail.com>
2019-04-03 21:32:53 +0800

    feat: add information

--- restful/app.py
+++ restful/app.py
@@ -1,10 +1,9 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
-
 from security import authenticate, identity
 
-app = Flask(__name__)
+app = Flask(__name__) #special python variable, unique name
 app.secret_key = 'akasht'
 api = Api(app)
 
