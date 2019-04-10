202767145c0303c958817587715c20a8c135426d
abbiyanaila <abbiyanaila@gmail.com>
2019-04-10 23:52:03 +0800

    feat: add information in every sintaks

--- sqlite/test.py
+++ sqlite/test.py
@@ -7,15 +7,16 @@ cursor = connection.cursor() #allow to select and start things, cursor (run quer
 create_table = "CREATE TABLE users (id int, username text, password text)" # create table
 cursor.execute(create_table) #run query create_table
 
-user = (1, ' desi', 'qwerty')
-insert_query = "INSERT INTO users VALUES (?, ?, ?)"
-cursor.execute(insert_query, user)
+user = (1, ' desi', 'qwerty') # fill in the database
+insert_query = "INSERT INTO users VALUES (?, ?, ?)" # insert the fill in
+cursor.execute(insert_query, user) # run query user and insert_query
 
+# new fill in database
 users = [
     (2, 'fandi', 'qwer'),
     (3, 'ratno', 'aswd')
 ]
-cursor.executemany(insert_query, users)
+cursor.executemany(insert_query, users) # run query
 
 select_query = "SELECT * FROM users"
 for row in cursor.execute(select_query):
